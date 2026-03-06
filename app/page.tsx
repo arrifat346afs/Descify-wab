564be5697eda15964373864a66931c5a4396afd7
arrifat346afs <arrifat1235@gmail.com>
2026-03-06 23:47:29 +0600

    feat: add HeroSection component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 // import { Button } from "@/components/ui/button"
 
+import HeroSection from "@/components/hero-section";
+
 export default function Page() {
   return (
     <div>
-       
+       <HeroSection />
     </div>
   )
 }
