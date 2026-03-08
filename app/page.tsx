09d6b23803e8c01201cfb31f42ec4517fc6ecfbd
arrifat346afs <arrifat1235@gmail.com>
2026-03-08 22:54:27 +0600

    feat(page): Add Features component import and usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 // import { Button } from "@/components/ui/button"
 
+import Features from "@/components/features-4";
 import HeroSection from "@/components/hero-section";
 
 export default function Page() {
   return (
     <div>
        <HeroSection />
-       <fet
+       <Features />
     </div>
   )
 }
