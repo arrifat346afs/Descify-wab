df672ec16b3a6f55094c35ccfddc525be4a0d3be
arrifat346afs <arrifat1235@gmail.com>
2026-03-08 23:04:35 +0600

    feat(page): Add Footer component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import { Button } from "@/components/ui/button"
 
 import Features from "@/components/features-4";
+import Footer from "@/components/footer-3";
 import HeroSection from "@/components/hero-section";
 
 export default function Page() {
@@ -8,6 +9,7 @@ export default function Page() {
     <div>
        <HeroSection />
        <Features />
+       <Footer />
     </div>
   )
 }
