539cb252b08af9db36350dd9fb52c0517ebd0d4a
arrifat346afs <arrifat1235@gmail.com>
2026-03-09 23:10:52 +0600

    refactor: Remove unused feature component imports and references

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 // import { Button } from "@/components/ui/button"
 
 import FAQsThree from "@/components/faqs-3"
-// import Features from "@/components/features-4";
 import FeaturesSectionDemo from "@/components/features-section-demo-2"
 import Footer from "@/components/footer-3"
 import HeroSection from "@/components/hero-section"
@@ -10,7 +9,6 @@ export default function Page() {
   return (
     <div>
       <HeroSection />
-      {/* <Features /> */}
       <div className="pt-20">
         <FeaturesSectionDemo />
       </div>
