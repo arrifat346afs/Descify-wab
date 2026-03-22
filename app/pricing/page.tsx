1d6ef59e76cdf603c37bc86b0fccebe9a2d876e3
arrifat346afs <arrifat1235@gmail.com>
2026-03-22 22:57:08 +0600

    feat(pricing-page): add page component rendering pricing section

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+// import Pricing from "@/components/pricing"
+import Pricing from "@/components/pricing"
+export const page = () => {
+  return (
+    <div>
+        <Pricing />
+    </div>
+  )
+}
