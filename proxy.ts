b177ea1c4e89768e53d8e1ce7a9142dedd8ad1e9
arrifat346afs <arrifat1235@gmail.com>
2026-03-22 23:38:17 +0600

    feat(proxy): Add createRouteMatcher and update middleware to protect auth for non-public routes

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/api/webhook(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isPublicRoute(req)) return;
+  await auth.protect();
+});
 
 export const config = {
   matcher: [
