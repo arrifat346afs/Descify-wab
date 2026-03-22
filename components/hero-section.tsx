c0a584f93ca5881f26d6cbbfce0ba78d2ec32ed1
arrifat346afs <arrifat1235@gmail.com>
2026-03-22 13:05:44 +0600

    refactor(hero-section): remove ModalImage component and add WavyBackground and HeaderSection imports

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -9,7 +9,6 @@ import { Github } from "lucide-react"
 import Image from "next/image"
 const GITHUB_IMAGE_URL =
   "https://raw.githubusercontent.com/arrifat346afs/Descify/main/img/Screenshot%20from%202026-03-21%2023-12-28.png"
-cGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAIABADASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAABgUH/8QAIhAAAgIBBAIDAAAAAAAAAAAAAQIDBBEABQYSITFBUWH/xAAVAQEBAAAAAAAAAAAAAAAAAAADBP/EABkRAAMBAQEAAAAAAAAAAAAAAAABAhEDIf/aAAwDAQACEQMRAD8AyW1trR3N7e2d9aR3MEV1LFJG8YZSjMCpUjocjB/ej9I6J0fR1zXg2Z5JJWlkYyM5LMxJJJJJJJPvRQA1HZsN3b2U1mu1jRkkjZ0Yq6hgAwI+oI6I+dGlbW1tW3bWtJ//2Q=="
 import { WavyBackground } from "./ui/wavy-background"
 
 import { HeaderSection } from "./HeaderSection"
