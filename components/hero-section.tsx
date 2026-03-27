33da34ba058b4ed0a3270d3547fcd08d57ed6a3b
arrifat346afs <arrifat1235@gmail.com>
2026-03-27 22:12:49 +0600

    fix(hero-section): update image URL to app.png

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -14,7 +14,7 @@ import { useState } from "react";
 
 
 const IMAGE_URL =
-  "https://ik.imagekit.io/gvt4qxkhg/nextjs%20/Screenshot%20from%202026-03-21%2023-12-28.png"
+  "https://ik.imagekit.io/gvt4qxkhg/nextjs%20/app.png"
 const Src = "/Screenshot from 2026-03-21 23-12-28.png"
 
 
