418a208f55c8486c4d964bb23b8d32276d5dd326
arrifat346afs <arrifat1235@gmail.com>
2026-03-28 14:19:26 +0600

    feat(tsparticles): add basic, engine, and interaction plugins dependencies

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,21 +6,19 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
 import { RiGithubFill } from "@remixicon/react"
-import { Image, ImageKitProvider, buildSrc } from '@imagekit/next';
+import { Image, ImageKitProvider, buildSrc } from "@imagekit/next"
 import { WavyBackground } from "./ui/wavy-background"
 
 import { HeaderSection } from "./HeaderSection"
-import { useState } from "react";
-
+import { useState } from "react"
+import { Cover } from "./ui/cover"
 
 const IMAGE_URL =
   "https://ik.imagekit.io/gvt4qxkhg/nextjs%20/Screenshot%20from%202026-03-27%2022-27-33.png"
 const Src = "/Screenshot from 2026-03-27 22-27-33.png"
 
-
-  
 export default function HeroSection() {
-   const [showPlaceholder, setShowPlaceholder] = useState(true);
+  const [showPlaceholder, setShowPlaceholder] = useState(true)
   return (
     <>
       <HeaderSection />
@@ -47,8 +45,8 @@ export default function HeroSection() {
                 AI metadata workflow for images
               </span>
               <h1 className="mx-auto mt-4 max-w-5xl text-3xl leading-tight font-semibold text-balance md:text-5xl lg:text-5xl xl:text-5xl">
-                Generate image Titles, Descriptions, and Keywords, Faster with
-                AI
+                Generate image Titles, Descriptions, and Keywords,{" "}
+                <Cover>Faster with AI</Cover>
               </h1>
               <p className="mx-auto my-6 max-w-3xl text-lg text-muted-foreground md:text-xl">
                 Supercharging your images with AI
@@ -79,60 +77,67 @@ export default function HeroSection() {
             <div className="pl-8 perspective-distant lg:pl-44">
               <div className="-mt-2 rotate-x-20 skew-x-12 mask-r-from-75% mask-b-from-55% mask-b-to-100% pt-2 pl-6 lg:-mt-10 lg:h-160">
                 <ImageKitProvider urlEndpoint={IMAGE_URL}>
-                <Image
-                  className="rounded-lg border shadow-xl dark:hidden"
-                  src={IMAGE_URL}
-                  alt="Descify desktop app preview"
-                  width={2880}
-                  height={2074}
-                  loading="eager"
-                    style={showPlaceholder ? {
-                      backgroundImage: `url(${buildSrc({
-                        urlEndpoint: IMAGE_URL,
-                        src: Src,
-                        transformation: [
-                          // {}, // Any other transformation you want to apply
-                          {
-                            quality: 10,
-                            blur: 90,
+                  <Image
+                    className="rounded-lg border shadow-xl dark:hidden"
+                    src={IMAGE_URL}
+                    alt="Descify desktop app preview"
+                    width={2880}
+                    height={2074}
+                    loading="eager"
+                    style={
+                      showPlaceholder
+                        ? {
+                            backgroundImage: `url(${buildSrc({
+                              urlEndpoint: IMAGE_URL,
+                              src: Src,
+                              transformation: [
+                                // {}, // Any other transformation you want to apply
+                                {
+                                  quality: 10,
+                                  blur: 90,
+                                },
+                              ],
+                            })})`,
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
                           }
-                        ]
-                      })})`,
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                    } : {}}
+                        : {}
+                    }
                     onLoad={() => {
-                      setShowPlaceholder(false);
+                      setShowPlaceholder(false)
                     }}
                   />
-                
-                <Image
-                  className="hidden rounded-lg border shadow-xl dark:block"
-                  src={IMAGE_URL}
-                  alt="Descify desktop app preview"
 
-                  width={2880}
-                  height={2074}
-                  loading="eager"
-                    style={showPlaceholder ? {
-                      backgroundImage: `url(${buildSrc({
-                        urlEndpoint: IMAGE_URL,
-                        src: Src,
-                        transformation: [
-                          // {}, // Any other transformation you want to apply
-                          {
-                            quality: 10,
-                            blur: 90,
+                  <Image
+                    className="hidden rounded-lg border shadow-xl dark:block"
+                    src={IMAGE_URL}
+                    alt="Descify desktop app preview"
+                    width={2880}
+                    height={2074}
+                    loading="eager"
+                    style={
+                      showPlaceholder
+                        ? {
+                            backgroundImage: `url(${buildSrc({
+                              urlEndpoint: IMAGE_URL,
+                              src: Src,
+                              transformation: [
+                                // {}, // Any other transformation you want to apply
+                                {
+                                  quality: 10,
+                                  blur: 90,
+                                },
+                              ],
+                            })})`,
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
                           }
-                        ]
-                      })})`,
-                      backgroundSize: "cover",
-                      backgroundRepeat: "no-repeat",
-                    } : {}}
+                        : {}
+                    }
                     onLoad={() => {
-                      setShowPlaceholder(false);
+                      setShowPlaceholder(false)
                     }}
-                />
+                  />
                 </ImageKitProvider>
               </div>
             </div>
